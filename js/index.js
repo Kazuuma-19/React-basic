0a976ea35491f34800f5cd6174affa6fadf45213
Kazuma Saito <kazu15870@gmail.com>
2022-11-19 10:46:07 +0900

    Day7 完了、削除機能を追加

--- js/index.js
+++ js/index.js
@@ -14,20 +14,49 @@ const onClickAdd = () => {
   //liタグの中に入力した値を設定
   li.innerText = inputText;
 
-  //button完了生成
+  //完了button生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+
   //完了ボタン機能
   completeButton.addEventListener("click", () => {
-    alert("complete");
+    //未完了から削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    // //完了したTODOlistの親要素divを取得
+    const addTarget = completeButton.parentNode;
+
+    //Todoタスク名を取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    //liタグ生成
+    const li = document.createElement("li");
+    //liタグの中に入力した値を設定
+    li.innerText = text;
+
+    //戻すボタン
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divの配下に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
-  //button削除生成
+  //削除button生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+
   //delete button function
   deleteButton.addEventListener("click", () => {
-    alert("delete");
+    //押された削除ボタンのdivを未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   //divの配下に各要素を設定
@@ -41,6 +70,10 @@ const onClickAdd = () => {
   console.log(deleteButton);
 }
 
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+}
 
 //add-buttonがクリックされたときに関数を実行
 document
