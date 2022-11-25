d15608fd5fab664b023185b21dcd58204e261320
Kazuma Saito <kazu15870@gmail.com>
2022-11-25 16:16:30 +0900

    start to create todo app by React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,7 @@
 //コンポーネントファイル
 import React from "react";
+import "./style.css";
 
-const App = () => {
-  const onClickButton = () => {
-    alert();
-  };
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px",
-  };
-
-  return (
-    //囲うだけ、意味無し
-    <>
-      <h1 style={{ color: "red" }}>hello</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
-    </>
-  );
+export const App = () => {
+  return <div></div>;
 };
-
-//他のファイルでつかうことを明示
-export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import { App } from './App'
 
 //画面に反映（何を,どこに）
 ReactDOM.render(<App />, document.getElementById("root"));
